b951f5b35a5af6ce536e0df97b953bedcc064305
Rahil Sama <rahil.sama@ucalgary.ca>
2025-05-15 13:17:39 -0400

    Created a functioning program to return median of sorted array

--- median-array.py
+++ median-array.py
@@ -17,17 +17,44 @@ class Solution:
         combinedArray = nums1 + nums2
         print(combinedArray)
         
-        p=0
-        q=1
-        r=2
+        p = 0
+        r = len(combinedArray)-1
         
-        sort = merge(combinedArray, p, q, r)
-        print(sort)
+        
+        mergeSort(combinedArray, p, r)
+        print(combinedArray)
+        if len(combinedArray)%2==0:
+            x = r//2
+            median = (combinedArray[x]+combinedArray[x+1])/2
+            return median
+        
+        else:
+            median = combinedArray[r//2]
+            return median
+        
         
         
 
+def mergeSort(arr: List[int], p, r):
+    # if len(arr) == 1:
+    #     return arr
+    # else:
+    #     p = 0
+    #     q = len(arr)//2
+    #     r = len(arr)-1
+    #     arr = merge(arr, p, q, r)
+    # print("Length if arr: ",len(arr))
+    # return mergeSort(arr)
+    if p < r:
+        q = (p+r)//2
+        mergeSort(arr, p, q)
+        mergeSort(arr, q+1, r)
+        merge(arr, p, q, r)
+        
+    
 
-def merge(A: List[int], p:int, q:int, r:int) -> List[int]:
+
+def merge(arr: List[int], p:int, q:int, r:int):
     n1 = q-p+1
     n2 = r-q
     
@@ -35,38 +62,37 @@ def merge(A: List[int], p:int, q:int, r:int) -> List[int]:
     rArr = []
     
     for i in range(n1):
-        lArr.append(A[p+i])
+        lArr.append(arr[p+i])
     print(lArr)
     for j in range(n2):
-        rArr.append(A[q+j+1])
+        rArr.append(arr[q+j+1])
     print(rArr)
     i=0
     j=0
     for k in range(p,r+1):
         if i >= len(lArr):
-            A[k] = rArr[j]
+            arr[k] = rArr[j]
             j+=1
         elif j >= len(rArr):
-            A[k] = lArr[i]
+            arr[k] = lArr[i]
             i+=1
         else:
             if lArr[i]<=rArr[j]:
-                A[k] = lArr[i]
+                arr[k] = lArr[i]
                 i+=1
             else:
-                A[k] = rArr[j]
+                arr[k] = rArr[j]
                 j+=1
         
-    return A    
     
     
 
 
 
 
-if __name__ == "__main__":
-    solution = Solution()
-    nums1 = [1, 3]
-    nums2 = [2]
-    median = solution.findMedianSortedArrays(nums1, nums2)
-    print("The median is:", median)
+# if __name__ == "__main__":
+#     solution = Solution()
+#     nums1 = [1, 3]
+#     nums2 = [4, 2]
+#     median = solution.findMedianSortedArrays(nums1, nums2)
+#     print("The median is:", median)
